735023fcd3290f7cd97d0a4033abed8d8edad67e
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-16 17:10:06 +0100

    step 13 finished

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,6 +9,7 @@ attr_reader :bike
 	end
 
 	def dock(bike)
+		fail 'Docking station full' if @bike
 		@bike = bike
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,21 +13,28 @@ describe DockingStation do
 			end
 	end
 
+	describe '#dock' do
+		it 'raises an error when the dock is full' do
+			subject.dock(Bike.new)
+			expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+		end
+
+		it 'docks something' do
+			bike = Bike.new
+			expect(subject.dock(bike)).to eq bike
+		end
+
+		it 'returns docked bikes' do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.bike).to eq bike
+		end
+	end
+
 	it 'gets a bike and expects it to be working' do
 		bike = Bike.new
 		subject.dock(bike)
 		expect(bike).to be_working
-	end
-
-	it 'docks something' do
-		bike = Bike.new
-		expect(subject.dock(bike)).to eq bike
-	end
-
-	it 'returns docked bikes' do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bike).to eq bike
 	end
 
 end
