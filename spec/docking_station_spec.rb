fd0db7eb3a54c063bc5f921ba74a5bc0dfcf83e0
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-16 15:50:29 +0100

    step 11 finished

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,4 @@
 require 'docking_station'
-require 'Bike'
 
 describe DockingStation do
 	it 'responds to release_bike' do
@@ -10,4 +9,16 @@ describe DockingStation do
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
+
+	it 'docks something' do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+	end
+
+	it 'returns docked bikes' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
 end
