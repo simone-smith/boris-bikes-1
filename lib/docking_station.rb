7b5e7e8b8f866aeb1d7805d898f2fd855bd063d6
Simone <simonecaplin@gmail.com>
2018-05-17 22:36:11 +0100

    First two user stories of exercise 18

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,7 +12,16 @@ class DockingStation
 
 	def release_bike
 		fail 'No bikes available' if empty?
+		fail 'No working bikes available' if @bikes.last.broken?
 		@bikes.pop
+		# If all bikes are broken, say 'No bikes available'
+		# if @bikes.all? {|bike| bike.broken?}
+		# 	fail 'No bikes available'
+		# Else, if there's a bike somewhere in the array that isn't broken, return it
+		# else
+		# 	working_bike = @bikes.index {|bike| bike.broken? == nil}
+		# 	released_bike = @bikes.delete_at(working_bike)
+		# end
 	end
 
 	def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -51,4 +51,11 @@ describe DockingStation do
 		expect(subject.release_bike).to be_working
 	end
 
+	it 'does not release a broken bike' do
+		bike = Bike.new
+		bike.report_broken
+		subject.dock(bike)
+		expect{ subject.release_bike }.to raise_error 'No working bikes available'
+	end
+
 end
